c38ddd22842e0e61d996a80647c630ca9c885de5
TonyToscana <xxpoblanozxx@gmail.com>
2022-01-12 11:09:32 +0100

    End of tutorial for project part1

--- part1/part1/src/App.js
+++ part1/part1/src/App.js
@@ -1,10 +1,34 @@
 import React from 'react';
 
-function App() {
+const Hello = (props) => {
   return (
     <div>
-      <p>Hello world</p>
+      <p>
+        Hello {props.name}, you are {props.age} years old
+      </p>
     </div>
+  );
+}
+
+const Footer = () => {
+  return (
+    <div>
+      greeting app created by <a href="https://github.com/TonyToscana">TonyToscana</a>
+    </div>
+  )
+}
+
+const App = () => {
+  const name = "Peter";
+  const age = 10;
+
+  return (
+    <>
+      <h1>Greetings</h1>
+      <Hello name="Maya" age={26 + 10}/>
+      <Hello name={name} age={age}/>
+      <Footer/>
+    </>
   );
 }
 
