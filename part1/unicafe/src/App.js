d20547fe51d52c9246fb622e553bf6fa68f23ffc
TonyToscana <xxpoblanozxx@gmail.com>
2022-01-27 13:25:47 +0100

    Spread props for statistics

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -25,13 +25,19 @@ const App = () => {
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
+  const statisticsProps = {
+    good,
+    neutral,
+    bad,
+  };
+
   return (
     <div>
       <h1>give feedback</h1>
       <button onClick={() => setGood(good + 1)}>good</button>
       <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
       <button onClick={() => setBad(bad + 1)}>bad</button>
-      <Statistics good={good} neutral={neutral} bad={bad} />
+      <Statistics {...statisticsProps} />
     </div>
   );
 };
