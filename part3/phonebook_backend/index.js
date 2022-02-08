937376235e3730be30676032a15631d8153c2a83
TonyToscana <xxpoblanozxx@gmail.com>
2022-02-08 12:21:46 +0100

    Exercise 3.6

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -62,6 +62,25 @@ app.delete('/api/persons/:id', (req, res) => {
 
 app.post('/api/persons', (req, res) => {
   const body = req.body;
+
+  if (!body.name) {
+    return res.status(400).json({
+      error: 'name missing',
+    });
+  }
+
+  if (!body.number) {
+    return res.status(400).json({
+      error: 'number missing',
+    });
+  }
+
+  if (data.findIndex((p) => p.name === body.name) >= 0) {
+    return res.status(400).json({
+      error: 'name must be unique',
+    });
+  }
+
   const person = {
     id: generateId(),
     name: body.name,
