b8d1f1dd949c42676d57f4778509c5e4c71962b4
TonyToscana <xxpoblanozxx@gmail.com>
2022-02-08 12:44:44 +0100

    Exercise 3.8

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -26,12 +26,22 @@ let data = [
   },
 ];
 
+morgan.token('body', (req, res) => {
+  if (req.method === 'POST') {
+    return JSON.stringify(req.body);
+  }
+
+  return '';
+});
+
 const generateId = () => {
   return Math.floor(Math.random() * 1000000);
 };
 
 app.use(express.json());
-app.use(morgan('tiny'));
+app.use(
+  morgan(':method :url :status :res[content-length] - :response-time ms :body')
+);
 
 app.get('/info', (req, res) => {
   res.send(
